22e3bcbfa3469b330a628174792dd1927e0a066d
azrizhaziq <azrizhaziq@gmail.com>
2019-01-07 09:58:58 +0800

    refactor open url into helpers

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const puppeteer = require('puppeteer')
+
 const getElementByXpath =
   path => document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
 
@@ -5,8 +7,20 @@ const getElementByXpathString = getElementByXpath.toString()
 
 const delay = (ms = 1000) => new Promise((resolve) => setTimeout(() => resolve(), ms))
 
+const open = async (link) => {
+  try {
+    const browser = await puppeteer.launch({ headless : false })
+    const page = await browser.newPage()
+    await page.goto(link)
+    return page
+  } catch(e) {
+    console.error('Error open page', e)
+  }
+}
+
 module.exports = {
   getElementByXpath,
   getElementByXpathString,
-  delay
+  delay,
+  open
 }
